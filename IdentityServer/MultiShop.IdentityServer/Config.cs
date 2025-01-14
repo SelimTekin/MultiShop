b72508869578d45dfd4e07341c4d8a385a72abf1
SelimTekin <tekinselim.57.57@gmail.com>
2025-01-14 23:47:01 +0300

    Order & Discount Permissions

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -7,29 +7,33 @@ using System.Collections.Generic;
 
 namespace MultiShop.IdentityServer
 {
-    public static class Config
-    {
-        // Property , IEnumerable -> Koleksiyon
-        public static IEnumerable<ApiResource> ApiResources => new ApiResource[] // örnek atadık
+	public static class Config
+	{
+		// Property , IEnumerable -> Koleksiyon
+		public static IEnumerable<ApiResource> ApiResources => new ApiResource[] // örnek atadık
         {
             // Her mikroservis için o mikroservise erişim sağlanacak bir key belirliyoruz.
             // ResourceCatalog ismindeki key'e sahip olan bir mikroservis kullanıcısı CatalogFullPermission işlemini gerçekleştirebilecek.
-            new ApiResource("ResourceCatalog"){Scopes = {"CatalogFullPermission", "CatalogReadPermission"}}
-        };
+            new ApiResource("ResourceCatalog"){Scopes = {"CatalogFullPermission", "CatalogReadPermission"}},
+            new ApiResource("ResourceDiscount"){Scopes = {"DiscountFullPermission"}},
+            new ApiResource("ResourceOrder"){Scopes = {"OrderFullPermission"}}
+		};
 
-        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
-        {
+		public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+		{
             // Token'ının aldığımız kullanıcının hangi yetkilere erişim sağlayacağını belirledik
             new IdentityResources.OpenId(),
-            new IdentityResources.Email(),
-            new IdentityResources.Profile(),
-        };
+			new IdentityResources.Email(),
+			new IdentityResources.Profile(),
+		};
 
-        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
-        { 
+		public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+		{ 
             // CatalogFullPermission'a sahip kişinin yapabileceği işlemler
             new ApiScope("CatalogFullPermission", "Full authority for catalog operations"),
-            new ApiScope("CatalogReadPermission", "Reading authority for catalog operations")
-        };
-    }
+			new ApiScope("CatalogReadPermission", "Reading authority for catalog operations"),
+			new ApiScope("DiscountFullPermission", "Full authority for catalog operations"),
+			new ApiScope("OrderFullPermission", "Full authority for catalog operations"),
+		};
+	}
 }
