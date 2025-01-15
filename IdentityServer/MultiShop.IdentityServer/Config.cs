337a46296d4b16c533dfd5049c0dc1b77a30fa8c
SelimTekin <tekinselim.57.57@gmail.com>
2025-01-15 15:37:33 +0300

    JWT Created

--- IdentityServer/MultiShop.IdentityServer/Config.cs
+++ IdentityServer/MultiShop.IdentityServer/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -34,6 +35,46 @@ namespace MultiShop.IdentityServer
 			new ApiScope("CatalogReadPermission", "Reading authority for catalog operations"),
 			new ApiScope("DiscountFullPermission", "Full authority for catalog operations"),
 			new ApiScope("OrderFullPermission", "Full authority for catalog operations"),
+		};
+
+		public static IEnumerable<Client> Clients => new Client[]
+		{
+			// Visitor -> Ziyaretçilerin sahip olacağı izinler
+			new Client
+			{
+				ClientId="MultiShopVisitorId",
+				ClientName="Multi Shop Visitor User",
+				AllowedGrantTypes=GrantTypes.ClientCredentials, // Neye izin verildiği ile ilgili bir property -> Kimlik işlemleri için kullanacağımız property
+				ClientSecrets={new Secret("multishopsecret".Sha256())}, // şifre
+				AllowedScopes={ "CatalogReadPermission" } // okuma yetkisi olsun sadece
+			},
+
+			// Manager
+			new Client
+			{
+				ClientId="MultiShopManagerId",
+				ClientName="Multi Shop Manager User",
+				AllowedGrantTypes=GrantTypes.ClientCredentials, // Neye izin verildiği ile ilgili bir property -> Kimlik işlemleri için kullanacağımız property
+				ClientSecrets={new Secret("multishopsecret".Sha256())}, // şifre
+				AllowedScopes={ "CatalogReadPermission", "CatalogFullPermission" }
+			},
+
+			// Admin
+			new Client
+			{
+				ClientId="MultiShopAdminId",
+				ClientName="Multi Shop Admin User",
+				AllowedGrantTypes=GrantTypes.ClientCredentials, // Neye izin verildiği ile ilgili bir property -> Kimlik işlemleri için kullanacağımız property
+				ClientSecrets={new Secret("multishopsecret".Sha256())}, // şifre
+				AllowedScopes={  "CatalogFullPermission", "CatalogReadPermission", "DiscountFullPermission", "OrderFullPermission",
+				IdentityServerConstants.LocalApi.ScopeName, // yerel deki apiden scope'un adına ulaş
+				IdentityServerConstants.StandardScopes.Email,
+				IdentityServerConstants.StandardScopes.OpenId,
+				IdentityServerConstants.StandardScopes.Profile,
+				},
+				AccessTokenLifetime=600 // 600 saniye (10 dk) token süresi
+			}
+
 		};
 	}
 }
