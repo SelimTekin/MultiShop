1710fdba87301136ca750a86e452ab96c3847097
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-02-19 21:47:22 +0300

    Cargo Controllers done

---
+++ Services/Cargo/MultiShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Cargo.BusinessLayer.Abstract;
+using MultiShop.Cargo.DtoLayer.Dtos.CargoDetailDtos;
+using MultiShop.Cargo.EntityLayer.Concrete;
+
+namespace MultiShop.Cargo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoDetailsController : ControllerBase
+    {
+        private readonly ICargoDetailService _cargoDetailService;
+
+        public CargoDetailsController(ICargoDetailService cargoDetailService)
+        {
+            _cargoDetailService = cargoDetailService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoDetailList()
+        {
+            var values = _cargoDetailService.TGetAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateCargoDetail(CreateCargoDetailDto createCargoDetailDto)
+        {
+            CargoDetail cargoDetail = new CargoDetail()
+            {
+                Barcode = createCargoDetailDto.Barcode,
+                CargoCompanyId = createCargoDetailDto.CargoCompanyId,
+                ReceiverCustomer = createCargoDetailDto.ReceiverCustomer,
+                SenderCustomer = createCargoDetailDto.SenderCustomer
+            };
+            _cargoDetailService.TInsert(cargoDetail);
+            return Ok("Kargo Detayı Başarıyla Oluşturuldu.");
+        }
+        [HttpDelete]
+        public IActionResult RemoveCargoDetail(int id)
+        {
+            _cargoDetailService.TDelete(id);
+            return Ok("Kargo Detayı Başarıyla Silindi.");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetCargoDetailById(int id)
+        {
+            var values = _cargoDetailService.TGetById(id);
+            return Ok(values);
+        }
+        [HttpPut]
+        public IActionResult UpdateCargoDetail(UpdateCargoDetailDto updateCargoDetailDto)
+        {
+            CargoDetail cargoDetail = new CargoDetail()
+            {
+                CargoDetailId = updateCargoDetailDto.CargoDetailId,
+                Barcode = updateCargoDetailDto.Barcode,
+                SenderCustomer = updateCargoDetailDto.ReceiverCustomer,
+                ReceiverCustomer = updateCargoDetailDto.SenderCustomer,
+                CargoCompanyId = updateCargoDetailDto.CargoCompanyId,
+            };
+            _cargoDetailService.TUpdate(cargoDetail);
+            return Ok("Kargo Detayı Başarıyla Güncellendi.");
+        }
+    }
+}
