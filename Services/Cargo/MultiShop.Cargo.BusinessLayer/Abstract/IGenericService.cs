32813dae5839b34458b71a5ca05781597ef43f5c
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-02-19 17:07:29 +0300

    Bussiness Layer Interfaces & Managers

---
+++ Services/Cargo/MultiShop.Cargo.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+        // TInsert -> T, business katmanından gelen metotlar olduğunu belirtir
+        void TInsert(T entity);
+        void TUpdate(T entity);
+        void TDelete(int id);
+        T TGetById(int id);
+        List<T> TGetAll();
+    }
+}
