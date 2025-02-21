192144908c9d76760197eab8e2e8a4335f0c35ee
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-02-21 22:56:01 +0300

    Basket Service

---
+++ Services/Basket/MultiShop.Basket/Services/BasketService.cs
@@ -0,0 +1,36 @@
+﻿using MultiShop.Basket.Dtos;
+using MultiShop.Basket.Settings;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MultiShop.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task DeleteBasket(string userId)
+        {
+            // KeyDeleteAsync -> redise özgü metod
+            await _redisService.GetDb().KeyDeleteAsync(userId);
+        }
+
+        public async Task<BasketTotalDto> GetBasket(string userId)
+        {
+            // id'ye göre sepet getirecek (StringGetAsync -> redise özgü)
+            var existbasket = await _redisService.GetDb().StringGetAsync(userId);
+            return JsonSerializer.Deserialize<BasketTotalDto>(existbasket);
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            // key - value şeklinde oluyor parametreler (StringSetAsync -> redise özgü metod)
+            await _redisService.GetDb().StringSetAsync(basketTotalDto.UserId, JsonSerializer.Serialize(basketTotalDto));
+        }
+    }
+}
