0391b52a16808008d31f1c7160d53fb8b4bd1e37
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-04-19 03:00:58 +0300

    Special Offer

---
+++ Services/Catalog/MultiShop.Catalog/Services/SpecialOfferServices/SpecialOfferService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using MultiShop.Catalog.Dtos.SpecialOfferDtos;
+using MultiShop.Catalog.Entities;
+
+namespace MultiShop.Catalog.Services.SpecialOfferServices
+{
+    public class SpecialOfferService : ISpecialOfferService
+    {
+        private readonly IMongoCollection<SpecialOffer> _specialOfferCollection;
+        private readonly IMapper _mapper;
+
+        public SpecialOfferService(IMongoCollection<SpecialOffer> specialOfferCollection, IMapper mapper)
+        {
+            _specialOfferCollection = specialOfferCollection;
+            _mapper = mapper;
+        }
+
+        public async Task CreateSpecialOfferAsync(CreateSpecialOfferDto SpecialOfferDto)
+        {
+            var value = _mapper.Map<SpecialOffer>(SpecialOfferDto);
+            await _specialOfferCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteSpecialOfferAsync(string id)
+        {
+            await _specialOfferCollection.DeleteOneAsync(x => x.SpecialOfferId == id);
+        }
+
+        public async Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync()
+        {
+            var values = await _specialOfferCollection.Find(x => true).ToListAsync();
+            return _mapper.Map<List<ResultSpecialOfferDto>>(values);
+        }
+
+        public async Task<GetByIdSpecialOfferDto> GetByIdSpecialOfferAsync(string id)
+        {
+            var values = await _specialOfferCollection.Find<SpecialOffer>(x => x.SpecialOfferId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdSpecialOfferDto>(values);
+        }
+
+        public async Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto specialOfferDto)
+        {
+            var values = _mapper.Map<SpecialOffer>(specialOfferDto);
+            await _specialOfferCollection.FindOneAndReplaceAsync(x => x.SpecialOfferId == specialOfferDto.SpecialOfferId, values);
+        }
+    }
+}
