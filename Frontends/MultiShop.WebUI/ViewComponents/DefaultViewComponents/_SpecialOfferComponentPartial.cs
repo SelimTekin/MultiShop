f8ac4ad426c5db31ce406d9e08945a78bcb39d84
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-04-22 17:43:21 +0300

    Special Offer Default Page

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpecialOfferComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _SpecialOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SpecialOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/SpecialOffers");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync(); // gelen veriyi string formatta oku
+                var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData); // json formatını metne çevir
+                return View(values);
+            }
             return View();
         }
     }
