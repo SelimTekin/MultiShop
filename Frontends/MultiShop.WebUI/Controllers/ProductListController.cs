d6b6af97eaf0e8982307dce168ee2399401b18a0
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-07-17 17:50:25 +0300

    Add comment functionality and UI updates
    - Updated `ProductListController` to handle comment submissions via a new `AddComment` action method.
    - Removed unnecessary closing div in `ProductDetail.cshtml`.
    - Replaced review section in `Default.cshtml` with a partial view for comments.
    - Created `AddComment.cshtml` for user input on comments, including fields for name, email, rating, and comment.

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,10 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace MultiShop.WebUI.Controllers
 {
 	public class ProductListController : Controller
 	{
-		public IActionResult Index(string categoryId)
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public IActionResult Index(string categoryId)
 		{
 			ViewBag.categoryId = categoryId;
 			return View();
@@ -13,7 +22,31 @@ namespace MultiShop.WebUI.Controllers
 		public IActionResult ProductDetail(string id)
 		{
 			ViewBag.productId = id;
-            return View();
+			return View();
+		}
+		[HttpGet]
+		public PartialViewResult AddComment()
+		{
+            return PartialView();
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+		{
+			createCommentDto.ProductId = "676c5f897a51ae9ff33adc2d";
+			createCommentDto.ImageUrl = "test";
+			createCommentDto.Rating = 1;
+			createCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+			createCommentDto.Status = false;
+
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createCommentDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7178/api/Comments", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Default");
+			}
+			return View();
 		}
 	}
 }
