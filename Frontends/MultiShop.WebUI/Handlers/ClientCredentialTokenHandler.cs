d2b9a431a9c7bfabbcbb7d7c1b3ea9f01093ebc6
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-08-12 10:12:41 +0300

    Add ClientCredentialTokenHandler and token service interface
    Introduce `ClientCredentialTokenHandler` to manage HTTP requests by adding authorization tokens. Implement `IClientCredentialTokenService` for asynchronous token retrieval, enhancing authentication token management in requests.

---
+++ Frontends/MultiShop.WebUI/Handlers/ClientCredentialTokenHandler.cs
@@ -0,0 +1,31 @@
+﻿using MultiShop.WebUI.Services.Interfaces;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace MultiShop.WebUI.Handlers
+{
+    public class ClientCredentialTokenHandler : DelegatingHandler
+    {
+        private readonly IClientCredentialTokenService _clientCredentialTokenService;
+        public ClientCredentialTokenHandler(IClientCredentialTokenService clientCredentialTokenService)
+        {
+            _clientCredentialTokenService = clientCredentialTokenService;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var accessToken = await _clientCredentialTokenService.GetToken();
+            // Add the access token to the request headers
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            var response = await base.SendAsync(request, cancellationToken);
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                // hata mesajı
+            }
+
+            return response;
+
+        }
+    }
+}
