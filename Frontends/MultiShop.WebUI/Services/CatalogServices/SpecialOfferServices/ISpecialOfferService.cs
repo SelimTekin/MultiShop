b67964f8d4174390cbc1c3010de117056263e0ec
Qjten0vE9P2vIsv3 <tekinselim.57.57@gmail.com>
2025-08-14 11:15:18 +0300

    Special Offer Converting

---
+++ Frontends/MultiShop.WebUI/Services/CatalogServices/SpecialOfferServices/ISpecialOfferService.cs
@@ -0,0 +1,13 @@
+﻿using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+
+namespace MultiShop.WebUI.Services.CatalogServices.SpecialOfferServices
+{
+    public interface ISpecialOfferService
+    {
+        Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync();
+        Task CreateSpecialOfferAsync(CreateSpecialOfferDto SpecialOfferDto);
+        Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto SpecialOfferDto);
+        Task DeleteSpecialOfferAsync(string id);
+        Task<UpdateSpecialOfferDto> GetByIdSpecialOfferAsync(string id);
+    }
+}
